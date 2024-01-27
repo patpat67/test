8332a67b4ba2ae9448816ff792fff314c516b573
patpat67 <76846279+patpat67@users.noreply.github.com>
2024-01-28 02:17:26 +0800

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -6,5 +6,5 @@ const CONFIGDATA = {
   buttonNo: "No",
   titleModar: "Thank you.",
   descriptionModar:
-    "Baobao thank you for picking me to be your valentine this and you know what? I expected it hehe cuzz i wuvv u ! I know I may not be the best in making u happy because there are also times where I've made you sad... But there will definitely be more happy moments than sad baobao! Regardless of all these things, If I were a cat, I’d spend all nine of my lives with you hehe <3 I love you baobaoo heheh ",
+    "Baobao thank you for picking me to be your valentine and you know what I kinda expected it and know that you wud accept it hehe cuzz i wuvv u and im v cute  ! I know I may not be the best in making u happy because there are also times where I've made you sad... But there will definitely be more happy moments than sad baobao! Regardless of all these things, If I were a cat, I’d spend all nine of my lives with you hehe <3 I love you baobaoo heheh ",
 };
