719e38c23f69ef240ecd89922399a6f193d93e79
patpat67 <76846279+patpat67@users.noreply.github.com>
2024-01-28 01:45:02 +0800

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
+  titleHeader: "Bao Bao I have something to announce. </br> Please be my valentine !!!",
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
-  buttonYes: "Tớ đồng ý",
-  buttonNo: "Không đồng ý",
-  titleModar: "Cảm ơn cậu.",
+    "Please give me your answer",
+  buttonYes: "Yes",
+  buttonNo: "No",
+  titleModar: "Thank you.",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    "Baobao thank you for picking me to be your valentine this year! I know I may not be the best in making u happy because there are also times where I've made you sad... But there will definitely be more happy than sad baobao! ",
 };
